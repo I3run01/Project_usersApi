5c247ea478ddbbab3793fa6b7ed916a6b0c4d47d
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-04-20 16:00:48 -0300

    c

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as cookieParser from "cookie-parser";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.enableCors();
   app.setGlobalPrefix('api')
   await app.listen(process.env.PORT || 3000);
 }
