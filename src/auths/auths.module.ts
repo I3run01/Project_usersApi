01c1bbdffc4db33f6209739e55b9476d8de96da3
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-04-21 15:20:59 -0300

    c

--- src/auths/auths.module.ts
+++ src/auths/auths.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './contransts'
 
 @Module({
   imports: [
     JwtModule.register({
       global: true,
-      secret: process.env.JWT_SECRET,
+      secret: jwtConstants.secret,
       signOptions: { expiresIn: '180d' },
     }),
   ],
---
+++ src/auths/contransts.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+    secret: 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY4MTQyNjM4OCwiaWF0IjoxNjgxNDI2Mzg4fQ.ziig15MUJYLH53N0WeTtRF_Xkllm_gQeRJxTN6UIUCo',
+}
